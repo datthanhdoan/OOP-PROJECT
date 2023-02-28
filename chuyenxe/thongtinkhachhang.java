318b207d5f973f6350181c23fa6f4c0b684422b2
Khang <vuduongkhang16@gmail.com>
2023-02-28 13:29:42 +0700

    da hoan chinh (them gi thi them)

---
+++ chuyenxe/thongtinkhachhang.java
@@ -0,0 +1,37 @@
+package chuyenxe;
+
+public class thongtinkhachhang {
+    private String name;
+    private int SDT;
+    private String DiaChi;
+
+    public thongtinkhachhang() {
+        this.name = name;
+        this.SDT = SDT;
+        this.DiaChi = DiaChi;
+    }
+
+    public String getnamekh() {
+        return name;
+    }
+
+    public void setnamekh(String name) {
+        this.name = name;
+    }
+
+    public int getSDTkh() {
+        return SDT;
+    }
+
+    public void setSDTkh(int SDT) {
+        this.SDT = SDT;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChikh(String DiaChia) {
+        this.DiaChi = DiaChia;
+    }
+}
